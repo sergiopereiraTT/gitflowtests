f66f6a171c710a5f082be4981fee14c6b847fd96
Sergio Pereira <sergio.pereira@tradingtechnologies.com>
2015-04-16 18:50:51 -0500

    Waterfall script

--- waterfall.py
+++ waterfall.py
@@ -1,22 +1,178 @@
 # -*- coding: utf-8 -*-
 import argparse
 import os
-from utils.io import exit_with_error
+from traceback import format_exc
+from utils.io import exit_with_error, print_error, print_warn
+import git
+import yaml
 
 args = None
 this_dir = os.path.abspath(os.path.dirname(__file__))
+repo = git.Repo('.')
 
 
-def validate_version(version):
+def run_git(command):
+    if isinstance(command, basestring):
+        command = command.split()
+    command.insert(0, 'git')
+    # print '==>', ' '.join(command)
     try:
-        return tuple(int(p) for p in version.split('.'))
+        return repo.git.execute(command)
+    except git.GitCommandError:
+        print_error('Git command failed.\n')
+        print_error(format_exc())
+        if in_conflict():
+            print_warn('You have conflicts that need to be resolved.')
+        exit_with_error('Aborting')
+
+
+def validate_version_new_name(version):
+    try:
+        parts = tuple(int(p) for p in version.split('.'))
+        if len(parts) < 3:
+            exit_with_error('Version number needs at least 3 parts, like 11.22.33')
     except ValueError:
         exit_with_error('Invalid version number: {}', version)
 
 
+def in_conflict():
+    resp = run_git('ls-files --unmerged')
+    return len(resp) > 0
+
+
+def ensure_working_tree_is_clean():
+    resp = run_git('status -s')
+    if len(resp) > 0:
+        exit_with_error('Your working directory contains changes. Stash, commit or undo them before retrying.')
+
+
+def merge_branch(src_branch, dst_branch):
+    print '    checking out', src_branch
+    repo.branches[src_branch].checkout()
+    assert src_branch == repo.active_branch.name
+    run_git('pull')
+
+    print '    checking out', dst_branch
+    repo.branches[dst_branch].checkout()
+    assert dst_branch == repo.active_branch.name
+    run_git('pull')
+
+    print '    merging', src_branch, 'into', dst_branch
+    run_git(['merge', '--no-ff', src_branch])
+    run_git('push')
+
+
+def delete_branch(branch_name):
+    # delete local and remote
+    print '    deleting local branch ', branch_name
+    run_git(['branch', '-d', branch_name])
+    print '    deleting remote branch', branch_name
+    run_git(['push', 'origin', '--delete', branch_name])
+
+
+def find_current_uat_branch():
+    branches = [b.name for b in repo.branches if b.name.startswith('uat/')]
+    if len(branches) != 1:
+        exit_with_error('Expected to find exactly one UAT branch but found {}: {}', len(branches), branches)
+    return branches[0]
+
+
+def find_current_release_branch():
+    branches = [b.name for b in repo.branches if b.name.startswith('release/')]
+    if len(branches) != 1:
+        exit_with_error('Expected to find exactly one Release branch but found {}: {}', len(branches), branches)
+    return branches[0]
+
+
+def finish_current_uat_branch():
+    print 'Retiring current UAT branch'
+    uat_branch = find_current_uat_branch()
+    release_branch = find_current_release_branch()
+    print '  found uat branch', uat_branch
+    print '  found release branch', release_branch
+    merge_branch(uat_branch, 'master')
+    merge_branch(uat_branch, release_branch)
+    delete_branch(uat_branch)
+
+
+def create_new_versioned_branch(branch_family, source_branch):
+    version = find_branch_version(source_branch)
+    name = '{}/{}'.format(branch_family, version)
+    print '    creating local branch ', name
+    repo.create_head(name, commit=source_branch)
+    print '    creating remote branch', name
+    run_git(['push', '-u', 'origin', name])
+
+
+def create_new_uat_branch():
+    print 'Creating new UAT branch'
+    rel_branch = find_current_release_branch()
+    create_new_versioned_branch('uat', rel_branch)
+
+
+def finish_current_release_branch():
+    print 'Retiring current Release branch'
+    release_branch = find_current_release_branch()
+    merge_branch(release_branch, 'develop')
+    delete_branch(release_branch)
+
+
+def create_new_release_branch():
+    print 'Creating new Release branch'
+    create_new_versioned_branch('release', 'develop')
+
+
+def update_develop_version(next_version):
+    print 'Updating version in develop branch to', next_version
+    version_data = read_version_data()
+    version_data['number'] = next_version
+    write_version_data(version_data)
+    repo.index.add(['version.yml'])
+    repo.index.commit('Update develop version to {}'.format(next_version))
+    run_git('push')
+
+
+def read_version_data():
+    version_file = os.path.join(this_dir, 'version.yml')
+    with open(version_file) as vf:
+        version_data = yaml.load(vf)
+    return version_data
+
+
+def write_version_data(version_data):
+    version_file = os.path.join(this_dir, 'version.yml')
+    with open(version_file, 'w') as vf:
+        yaml.safe_dump(version_data, vf, default_flow_style=False)
+
+
+def find_branch_version(branch_name):
+    prev_branch = repo.active_branch.name
+    if prev_branch != branch_name:
+        repo.branches[branch_name].checkout()
+    version_data = read_version_data()
+    version = version_data['number']
+    if prev_branch != branch_name:
+        repo.branches[prev_branch].checkout()
+    return version
+
+
+def tag_master_version():
+    print 'Tagging master version'
+    version = find_branch_version('master')
+    print '  tag will be', version
+    repo.create_tag(version, repo.branches.master.commit.hexsha)
+    run_git(['push', 'origin', version])
+
+
 def main():
-    validate_version(args.next_dev_version)
-    # this line pretends to be a hotfix in the uat branch
+    validate_version_new_name(args.next_dev_version)
+    ensure_working_tree_is_clean()
+    finish_current_uat_branch()
+    tag_master_version()
+    create_new_uat_branch()
+    finish_current_release_branch()
+    create_new_release_branch()
+    update_develop_version(args.next_dev_version)
 
 
 if __name__ == '__main__':
